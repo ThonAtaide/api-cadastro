bdde87e6c307b3244e8e1438f72ae19d2731034f
ThonAtaide <thon_ataide10@hotmail.com>
2021-11-08 00:28:15 -0300

    [API-005] - updated config and docker compose to works with env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,14 +7,21 @@ export interface ISettings {
 
 export default (): ISettings => {
     if (process.env.NODE_ENV === 'production') {
+        console.log(`Running in Production`);
+        const database_host: string = process.env.DATABASE_HOST as string;
+        let database_url: string = process.env.DATABASE_URL as string;
+        console.log(`Database connected url: ${database_url.replace('?', database_host)}`)
         return {
-            DATABASE_URL: process.env.DATABASE_URL as string,
+            DATABASE_URL: database_url.replace('?', database_host) as string,
             PORT: process.env.PORT as string,
             SECRET_KEY: process.env.SECRET_KEY as string
         }
     }
+    console.log('Running on development mode.');
+    const database_host: string = process.env.DATABASE_HOST as string;
+    let database_url: string = process.env.DATABASE_URL as string || `postgres://postgres:root@localhost:5432/APICADASTRO`;
     return {
-        DATABASE_URL: process.env.DATABASE_URL as string || 'postgres://postgres:root@localhost:5432/APICADASTRO',
+        DATABASE_URL: database_host? database_url.replace('?', 'database_host'): database_url,
         PORT: process.env.PORT as string || '5000',
         SECRET_KEY: process.env.SECRET_KEY as string || 'dsjhdsgdsdsgjshjhskjdgshjdsfsagghsfhs'
     }
